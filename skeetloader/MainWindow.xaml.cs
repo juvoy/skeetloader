17509d01285e434fcaffbd010e18d277fb81a873
johng0tti <drogenjunk420@gmail.com>
2024-08-20 11:57:39 +0200

    added login screen and icon

--- skeetloader/MainWindow.xaml.cs
+++ skeetloader/MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace skeetloader
         {
             bSelected = true;
             InitializeComponent();
+
+            main_grid.Visibility = Visibility.Hidden;
+            login_grid.Visibility = Visibility.Visible;
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -43,6 +46,7 @@ namespace skeetloader
             Run r = new Run("Waiting for CS:GO");
             r.Foreground = new SolidColorBrush(Color.FromRgb(80, 118, 32));
 
+
             StatusBox.Document.Blocks.Add(new Paragraph(r));
         }
 
@@ -54,6 +58,44 @@ namespace skeetloader
 
             Rectangle bg = CS_BG;
             bg.Fill = bSelected ? new SolidColorBrush(Color.FromRgb(26, 26, 26)) : new SolidColorBrush(Color.FromRgb(35, 35, 35));
+
+            Launch_Button.IsEnabled = bSelected;
+        }
+
+        private void Login_Button_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            main_grid.Visibility = Visibility.Visible;
+            login_grid.Visibility = Visibility.Hidden;
+        }
+
+        private void username_box_capture(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        private void username_hint(object sender, MouseButtonEventArgs e)
+        {
+            username_box.CaptureMouse();
+        }
+
+        private void username_box_lost_keyboard(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(username_box.Text))
+            {
+     }
+        }
+
+        private void password_hint(object sender, MouseButtonEventArgs e)
+        {
+            password_box.CaptureMouse();
+        }
+
+        private void password_box_capture(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        private void password_box_lost_keyboard(object sender, KeyboardFocusChangedEventArgs e)
+        {
+
         }
     }
 }
